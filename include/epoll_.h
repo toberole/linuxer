1bd6e11f90ff6d07cc8b8e8543ee0c3f1096c6aa
zhouwei215664 <zhouwei215664@sogou-inc.com>
2019-01-11 16:40:26 +0800

    epool

---
+++ include/epoll_.h
@@ -0,0 +1,8 @@
+//
+// Created by Apple on 2019/1/11.
+//
+
+#ifndef LINUXER_EPOLL_H
+#define LINUXER_EPOLL_H
+
+#endif //LINUXER_EPOLL_H
---
+++ src/epoll_.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Apple on 2019/1/11.
+//
+
+#include "epoll_.h"
+
