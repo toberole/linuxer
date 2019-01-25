a1b34d1d4bb5c119bd827be1bc975e39eae9d40b
zhouwei215664 <zhouwei215664@sogou-inc.com>
2019-01-25 19:25:19 +0800

    review

--- demo/demo01.cpp
+++ demo/demo01.cpp
@@ -8,22 +8,34 @@
 #include <string>
 
 //中文字符
-void test_demo01(){
+void test_demo01() {
     std::wstring wstr = L"中文字符";
-    printf("len: %ld\n",wstr.size());
+    printf("len: %ld\n", wstr.size());
 
     std::wstring wstr1(wstr.c_str());
-    printf("%s",wstr1);
+    printf("%s", wstr1);
 }
 
+// typedef一个Func的函数指针
+typedef void (*Func)();
+
+
+void test_demo02() {
+    printf("test_demo02\n");
+}
+
+int test_demo03(int a) {
+    printf("test_demo03 a = %d\n", a);
+}
 
 #ifdef DEMO01
 
 int main(int argc,char ** args){
 
-    test_demo01();
+    // test_demo01();
 
-
+    Func f = test_demo02;
+    f();
 
     printf("press any key to exit");
     getchar();
